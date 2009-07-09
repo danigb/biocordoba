9f7d6f7a82316471df4db7074beab309f9ece0e9
Juan Gallego <juan@beecoder.com>
2009-07-09 11:24:08 +0200

    File config/config.yml required to start app

--- config/initializers/load_config.rb
+++ config/initializers/load_config.rb
@@ -1,4 +1,10 @@
-raw_config = File.read(RAILS_ROOT + "/config/config.yml")
+begin
+  raw_config = File.read(RAILS_ROOT + "/config/config.yml")
+rescue Errno::ENOENT
+  puts "Error! You MUST configure config/config.yml file"
+  exit
+end
+
 CONFIG = YAML.load(raw_config)[RAILS_ENV]
 
 puts "config file loaded"
