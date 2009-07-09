24f3d62f34e1772048f092dfbd34a1420eee68ae
Miguel Ángel Martínez Triviño <miguelangel@beecoder.com>
2009-07-09 12:23:51 +0200

    Instalando gettext y poniendo selector de idioma de prueba

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,15 @@ class ApplicationController < ActionController::Base
   include AuthenticatedSystem
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
-  before_filter :login_required
+  before_filter :set_gettext_locale, :login_required
 
   # Scrub sensitive parameters from your log
   filter_parameter_logging :password
+
+  def set_gettext_locale
+    FastGettext.text_domain = 'app'
+    FastGettext.available_locales = ['es','en'] #all you want to allow
+    # FastGettext.default_locale = 'es'
+    super
+  end
 end
---
+++ config/initializers/fast_gettext.rb
@@ -0,0 +1,1 @@
+FastGettext.add_text_domain 'app', :path => File.join(RAILS_ROOT, 'locale')
