57e41929c76e07064748a15628cfeee652433c8c
Juan Gallego <juan@beecoder.com>
2009-07-19 18:29:32 +0200

    Location removed.

--- db/migrate/20090718092719_add_location_id_to_users.rb
+++
@@ -1,9 +0,0 @@
-class AddLocationIdToUsers < ActiveRecord::Migration
-  def self.up
-    add_column :users, :location_id, :integer
-  end
-
-  def self.down
-    remove_column :users, :location_id
-  end
-end
