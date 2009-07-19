15accfbaf017f1106493b383d322861f5334e1a8
Juan Gallego <juan@beecoder.com>
2009-07-19 20:52:59 +0200

    Users in admin namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,19 @@
 ActionController::Routing::Routes.draw do |map|
   map.resources :preferences
-
   map.resources :sectors
 
   map.root :controller => 'main'
   map.logout '/logout', :controller => 'sessions', :action => 'destroy'
   map.login '/login', :controller => 'sessions', :action => 'new'
-  map.register '/register', :controller => 'users', :action => 'create'
-  map.signup '/signup', :controller => 'users', :action => 'new'
+  map.register '/admin/users/register', :controller => 'Admin::Users', :action => 'create'
+  map.signup '/admin/users/registro', :controller => 'Admin::Users', :action => 'new'
 
-  map.type_users '/users/type/:type', :controller => 'users', :action => 'type'
-
-  map.resources :users
   map.resource :session
 
+  map.type_users '/admin/users/type/:type', :controller => 'Admin::Users', :action => 'type'
   map.namespace :admin do |admin|
     admin.resources :preferences
+    admin.resources :users
   end
 
   # map.root :controller => "welcome"
