dea4bfdb30aed8cfd911ac10e92292ec9a22176c
Miguel Ángel Martínez Triviño <miguelangel@beecoder.com>
2009-07-20 10:52:25 +0200

    Listando preferencias

--- config/initializers/breadcrumbs.rb
+++ config/initializers/breadcrumbs.rb
@@ -2,12 +2,15 @@ Breadcrumb.configure do
   # Specify name, link title and the URL to link to
   crumb :admin_root, "Administración", :admin_url
   crumb :admin_users, "Usuarios", :admin_users_url
+  crumb :admin_users_type, '#{params[:type]}', :type_users_url, :params => :type
   crumb :admin_preferences, "Preferencias", :admin_preferences_url
 
   # Specify controller, action, and an array of the crumbs you specified above
-  trail "Admin::Main", :show, [:admin_root]
-  trail "admin/users", :index, [:admin_root, :admin_users]
-  trail "admin/preferences", :index, [:admin_root, :admin_preferences]
+  trail "admin/users", :index, [:admin_root]
+  trail "admin/preferences", :index, [:admin_root]
+  trail "Admin::Users", :type, [:admin_root, :admin_users]
+  trail "Admin::Users", :new, [:admin_root]
+  trail "admin/preferences", [:new, :edit], [:admin_root, :admin_preferences]
 
   # Specify the delimiter for the crumbs
   delimit_with " - "
