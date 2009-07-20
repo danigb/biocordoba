5c3eda518706490fa6bb7f423628cc0bdd53996f
Miguel Ángel Martínez Triviño <miguelangel@beecoder.com>
2009-07-20 16:38:27 +0200

    Modificando users new con jquery

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,4 +1,5 @@
 class Profile < ActiveRecord::Base
   belongs_to :user
   belongs_to :sector, :counter_cache => true
+  validates_presence_of :company_name, :address, :sector_id
 end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,6 +2,25 @@
 // This file is automatically included by javascript_include_tag :defaults
 
 $(document).ready(function() { 
+
   $("#orderable").tablesorter(); 
+
+  //Rol, formulario nuevo usuario
+  $("select#user_role_id").bind("change", function(e){
+
+    for(i in a = ['buyer', 'exhibitor']){
+      $("#"+a[i]).hide();
+    }
+
+    var value = $("select#user_role_id").val();
+    if(value == 3 || value == 4){
+      $("#buyer").show();
+    }else if(value == 5 ){
+      $("#exhibitor").show();
+    }
+  });
+
+
 });
+
 
