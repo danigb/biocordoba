471cc64de5e02e72fe0ae0cbc6d76d9301e86f35
Juan Gallego <juan@beecoder.com>
2009-07-28 10:21:26 +0200

    Unique company name

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,4 +2,5 @@ class Profile < ActiveRecord::Base
   belongs_to :user
   belongs_to :sector, :counter_cache => true
   validates_presence_of :company_name, :address, :sector_id
+  validates_uniqueness_of :company_name
 end
