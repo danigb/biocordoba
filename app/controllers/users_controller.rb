d287452fb39d91150d9453cf14949cf1cd0c1610
Miguel Ángel Martínez Triviño <miguelangel@beecoder.com>
2009-07-29 23:34:30 +0200

    Modificando el buscador

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,9 @@
 class UsersController < ApplicationController
   def search
-    unless (buyers = User.buyers).blank?
-      @search = User.buyers.search(params[:search])
-      @users = @search.ascend_by_profile_company_name.all
+    # unless (buyers = User.buyers).blank?
+      @search = User.search(params[:search]).roles_title_like_any(["international_buyer", "national_buyer", "exhibitor"])
+      @users = @search.ascend_by_profile_company_name.all(:include => [{:profile => :sector}, :roles])
       @users_by_sector = @users.group_by{|u| u.profile.sector.name }
-    end
+    # end
   end
 end
--- app/models/role.rb
+++ app/models/role.rb
@@ -3,4 +3,6 @@ class Role < ActiveRecord::Base
 
   validates_presence_of :title
   validates_uniqueness_of :title
+
+  named_scope :public_roles, :conditions => ["title != 'admin' && title != 'extenda'"]
 end
