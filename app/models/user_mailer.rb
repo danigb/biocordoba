5eac270dc2604fc0e456de195dae04b94507fe81
Juan Gallego <juan@beecoder.com>
2009-08-01 10:29:36 +0200

    Welcome email fixed

--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -1,9 +1,9 @@
 class UserMailer < ActionMailer::Base
   def welcome_email(current_user, new_user)  
-    recipients [current_user.email, new_user.email]
+    recipients [current_user.email, new_user.email].compact
     from "Awesome Site Notifications <notifications@example.com>"  
     subject "[AndalucíaSabor] Nuevo usuario"  
     sent_on Time.now 
-    body( {:user => new_user, :password => new_user.password, :url => "http://example.com/login"})  
+    body( {:user => new_user, :url => "http://example.com/login"})  
   end   
 end
