ed12ae7d0b71aa3b9590890b90ceeaf1097db8e3
Miguel Ángel Martínez Triviño <miguelangel@beecoder.com>
2009-08-05 11:48:23 +0200

    Email Nuevo mensaje recibido

--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -1,12 +1,25 @@
 class UserMailer < ActionMailer::Base
+  layout 'email'
+
   def welcome_email(current_user, new_user)  
-    recipients [current_user.email, new_user.email].compact
-    from "Andalucía Sabor International Fine Food Exhibition <andaluciasabor@andaluciasabor.es>"  
-    subject "[Andalucía Sabor] Nuevo usuario"  
-    sent_on Time.now 
-    body( {:user => new_user, :url => "http://example.com/login"})  
+    setup_email
+    @recipients = [current_user.email, new_user.email].compact
+    @subject += "Nuevo usuario"  
+    @body = {:user => new_user, :url => "http://example.com/login"}
   end   
 
   #Nuevo mensaje recibido
+  def new_message_received(receiver, message)
+    setup_email
+    @recipients = receiver.email
+    @subject += "Nuevo mensaje recibido"  
+    @body = {:sender => message.sender, :message => message}
+  end
 
+  def setup_email
+    @from = "Andalucía sabor international fine food exhibition <andaluciasabor@andaluciasabor.es>"  
+    @subject = "[Andalucía Sabor] "  
+    @sent_on = Time.now 
+    content_type = "text/html"
+  end
 end
