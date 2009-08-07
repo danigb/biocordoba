23d71bd1b129b6bbeabed01be013eb16fed45f8c
Miguel Ángel Martínez Triviño <miguelangel@beecoder.com>
2009-08-07 11:00:15 +0200

    Creating Event class in /lib dir

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,8 +1,8 @@
 class MainController < ApplicationController
   def home
     unless current_user.is_admin_or_extenda?
-      @date = Time.parse("#{CONFIG[:admin][:preferences][:event_start_day]} #{CONFIG[:admin][:preferences][:event_day_start_at]}")
-      @days = 3
+      @date = Event.start_day_and_hour
+      @days = Event.duration
       @meetings = current_user.meetings(@date, @days)
     end
   end
---
+++ lib/event.rb
@@ -0,0 +1,38 @@
+#Clase que se encarga de representar el objeto evento
+class Event
+  #Fecha inicio del evento
+  def self.start_day
+    Date.parse(CONFIG[:admin][:preferences][:event_start_day])
+  end
+
+  #Hora inicio evento
+  def self.day_start_hour
+    Date.parse(CONFIG[:admin][:preferences][:event_day_start_at])
+  end
+
+  #Hora y fecha inicio del evento
+  def self.start_day_and_hour
+    Time.parse("#{CONFIG[:admin][:preferences][:event_start_day]} #{CONFIG[:admin][:preferences][:event_day_start_at]}")
+  end
+
+  #Fecha fin evento
+  def self.end_day
+    Date.parse(CONFIG[:admin][:preferences][:event_end_day])
+  end
+
+  #Número días duración evento
+  def self.duration
+    end_day.day - start_day.day + 1
+  end
+
+  #Array con las fechas que comprende el evento
+  def self.days
+    res = []
+    Event.duration.times do |i|
+      res << Event.start_day + i
+    end
+    res
+
+  end
+
+end
