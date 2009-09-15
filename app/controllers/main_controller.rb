1e2afa46cfab142f01b812333782b0c5322c9b80
Laura <laura@macbook-de-laura.local>
2009-09-15 11:01:54 +0200

    Welcome email changed

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -9,10 +9,10 @@ class MainController < ApplicationController
 
   def email
     @date = Event.start_day_and_hour
-    @user = User.find_by_login("cocacola")
+    @user = User.first
     @meetings = @user.meetings(@date,1)
     @meeting= @meetings.first 
     @message = Message.first 
-    render :file => "meeting_mailer/#{params[:id]}", :layout => "email"
+    render :file => "user_mailer/#{params[:id]}", :layout => "email"
   end
 end
