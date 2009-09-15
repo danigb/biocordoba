0fd9c02f82b275713cc175f0f9c364b06a2eb26b
Miguel Ángel Martínez Triviño <miguelangel@beecoder.com>
2009-09-15 12:15:59 +0200

    obs

--- app/models/meeting_observer.rb
+++ app/models/meeting_observer.rb
@@ -5,7 +5,7 @@ class MeetingObserver < ActiveRecord::Observer
     if record.guest.is_national_buyer?  
       record.accept!
       #Email nueva cita
-      MeetingMailer.send_later(:deliver_new_meeting, record) id record.guest.email.present?
+      MeetingMailer.send_later(:deliver_new_meeting, record) if record.guest.email.present?
     elsif record.guest.is_international_buyer?
       #Aviso a los usuarios extenda
       MeetingMailer.send_later(:deliver_alert_for_extenda, record) if record.guest.email 
