2b7e0c2600b7fe4e63316b82da747574d8cfab98
Miguel Ángel Martínez Triviño <miguelangel@beecoder.com>
2009-09-15 18:41:29 +0200

    Removing email

--- app/models/country.rb
+++ app/models/country.rb
@@ -2,4 +2,6 @@ class Country < ActiveRecord::Base
   validates_presence_of :name, :code
 
   default_scope :order => 'name'
+
+  named_scope :with_profiles, :conditions => ["profiles_count > 0"]
 end
