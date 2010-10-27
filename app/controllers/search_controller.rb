a0b3a57c9878a31183a329796b952cc6607700c7
danigb <danigb@gmail.com>
2010-10-27 14:51:58 +0200

    search bug fixed. print safe color

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -12,12 +12,13 @@ class SearchController < ApplicationController
   private
   def find_users
     @sectors = Sector.find(:all, :select => 'id, name')
-    profile_company_name = params[:search] ? params[:search][:profile_company_name_like] : ''
-    @search = User.search()
-    @search.profile_company_name_like(profile_company_name.gsub("."," "))
+    @search = User.search
+    if params[:search]
+      @search.profile_company_name_like(params[:search][:profile_company_name_like].gsub("."," ")) if params[:search][:profile_company_name_like].present?
+      @search.profile_sectors_id_equals(params[:search][:profile_sectors_id_equals]) if params[:search][:profile_sectors_id_equals].present?
+    end
     @search.roles_title_like_any(current_user.is_exhibitor? ? ["international_buyer", "national_buyer"] : ["exhibitor"])
     @search.state_equals("enabled")
-    #paginate(:per_page => 20, :page => params[:page], :order => params[:sort])
     @users = @search.paginate(:include => [{:profile => [:sectors, :country]}, :roles], :per_page => 20, :page => params[:page])
     @users_by_sector = @users.group_by{|u| u.profile.sectors.first.name }
     @users_by_sector = @users.group_by{|u| u.profile.country.name }
