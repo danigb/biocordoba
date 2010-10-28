c9274557c9b97fa68fd5aeb4c5ce409298a6016a
danigb <danigb@gmail.com>
2010-10-28 13:34:16 +0200

    print calendar

--- app/helpers/print_helper.rb
+++ app/helpers/print_helper.rb
@@ -1,5 +1,18 @@
 module PrintHelper
   def print_data(label, value)
-    "<p><label>#{label}</label>#{value}</p>" #if value.present?
+    "<p class='print-data'><label>#{label}</label>#{value}</p>" #if value.present?
   end
+
+  def print_calendar(objects, options = {})
+    date = options[:date] || Time.now
+    options[:days] ||= 1
+    start_date = Date.new(date.year, date.month, date.day)
+    end_date = Date.new(date.year, date.month, date.day) + (options[:days] - 1)
+    week = (options[:days]) == 1 ? "mini_week" : "week"
+    week = "mini_without_hour_week" unless options[:without_hours].blank?
+    concat(tag("table", :id => week))
+    yield WeeklyPrinter.new(objects || [], self, options, start_date, end_date)
+    concat("</table>")
+  end
+
 end
