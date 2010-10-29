131883c66305733015d63adf1de290cda8197daf
danigb <danigb@gmail.com>
2010-10-29 11:23:54 +0200

    colores y algo de impresión. mensaje antes de imprimir

---
+++ public/javascripts/print.js
@@ -0,0 +1,5 @@
+(function($) {
+  $(function() {
+    alert("Para una mejor impresión, configura tu navegador para que imprima colores de fondo. Lo puedes hacer en Configurar Página del menú Archivo");
+  });
+})(jQuery);
